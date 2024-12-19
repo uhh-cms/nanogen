209ab96f94ceea16bc2e3b5188947ddb9def6b73
Marcel R. <github.riga@icloud.com>
2024-12-19 19:13:23 +0100

    Update law, fixes mirrored targets on maxwell.

--- nanogen/tasks/nano.py
+++ nanogen/tasks/nano.py
@@ -163,7 +163,7 @@ class CreateNano(NanoDatasetWorkflow, CMSSWSandboxTask):
         "'run_number,event_number[,end_event_number]'; empty default",
     )
     max_runtime = NanoDatasetWorkflow.max_runtime.copy(
-        default=6,  # hours
+        default=8,  # hours
         add_default_to_description=True,
     )
     htcondor_memory = NanoDatasetWorkflow.htcondor_memory.copy(
